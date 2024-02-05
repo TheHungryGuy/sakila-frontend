d9875395dd7b90be290a07ae9b634cb886d93789
Antonio Arce <antoarcejr@gmail.com>
2024-02-04 23:11:17 -0500

    Home Page Layout laid out

--- src/components/feature_actors/ActorList.jsx
+++ src/components/feature_actors/ActorList.jsx
@@ -1,32 +1,65 @@
 import React from "react";
+import { useState } from "react";
+import { Button, Modal } from "flowbite-react";
+
 import ActorCard from "./ActorCard";
 
 const ActorList = () => {
   const actors = [
-    { actorName: "Actor 1", MovieCount: 20 },
-    { actorName: "Actor 2", MovieCount: 15 },
-    { actorName: "Actor 3", MovieCount: 10 },
-    { actorName: "Actor 4", MovieCount: 8 },
-    { actorName: "Super Long Actor 5", MovieCount: 5 },
+    { actorName: "Actor 1", movieCount: 20 },
+    { actorName: "Actor 2", movieCount: 15 },
+    { actorName: "Actor 3", movieCount: 10 },
+    { actorName: "Actor 4", movieCount: 8 },
+    { actorName: "Super Long Actor 5", movieCount: 5 },
     // Test Actor List
   ];
 
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedActor, setSelectedActor] = useState({
+    actorName: "",
+    movieCount: 0,
+  });
+
+  const handleSeeMoreClick = (actor) => {
+    setSelectedActor(actor);
+    setOpenModal(true);
+  };
+
   return (
-    <div className="w-full py-[10rem] px-4 bg-[#000300] flex flex-col gap-24">
+    <div className="w-full pt-[4rem] py-[10rem] px-4 bg-[#000300] flex flex-col gap-24">
+      <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+        <Modal.Header>{selectedActor.actorName}</Modal.Header>
+        <Modal.Body>
+          <div className="space-y-6">
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Top Films
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              {selectedActor.movieCount}
+            </p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button color="warning" onClick={() => setOpenModal(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div className="order-first">
         <header className="text-center font-bold text-6xl text-white ">
           Top 5 Actors
         </header>
       </div>
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-5 gap-8">
-        {actors.map((Actor, index) => (
+        {actors.map((actor, index) => (
           <ActorCard
             key={index}
-            actorName={Actor.actorName}
-            movieCount={Actor.MovieCount}
-            onSeeMoreClick={() =>
-              console.log(`See more details for ${Actor.actorName}`)
-            }
+            actorName={actor.actorName}
+            movieCount={actor.movieCount}
+            onSeeMoreClick={() => {
+              console.log(`See more details for ${actor.actorName}`);
+              handleSeeMoreClick(actor);
+            }}
           />
         ))}
       </div>
--- src/components/feature_movies/MovieList.jsx
+++ src/components/feature_movies/MovieList.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useState } from "react";
 import MovieCard from "./MovieCard";
+import { Button, Modal } from "flowbite-react";
 
 const MovieList = () => {
   const movies = [
@@ -11,8 +13,52 @@ const MovieList = () => {
     // Test Movies list
   ];
 
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState({
+    title: "",
+    rentalCount: 0,
+  });
+
+  const handleSeeMoreClick = (movie) => {
+    setSelectedMovie(movie);
+    setOpenModal(true);
+  };
+
   return (
-    <div className="w-full py-[10rem] px-4 bg-white flex flex-col gap-24">
+    <div className="w-full pt-[4rem] py-[10rem] px-4 bg-white flex flex-col gap-24">
+      <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+        <Modal.Header>{selectedMovie.title}</Modal.Header>
+        <Modal.Body>
+          <div className="space-y-6">
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Rental Count: {selectedMovie.rentalCount}
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Description: ipsum Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Omnis accusamus ad, qui ipsam nesciunt nam
+              minima modi voluptates debitis quam, iste fugit sed eligendi
+              laboriosam, voluptate perferendis accusantium quia soluta. Year
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Release: 20XX
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Rating: PG-13
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Special Features: Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ipsam quos voluptates quod quas dicta omnis
+              eligendi sit commodi non quisquam nisi, harum inventore cumque
+              ipsum architecto tempora nihil. Officia, explicabo.
+            </p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button color="warning" onClick={() => setOpenModal(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div className="order-first">
         <header className="text-center font-bold text-6xl ">
           Top 5 Movies
@@ -24,9 +70,10 @@ const MovieList = () => {
             key={index}
             title={movie.title}
             rentalCount={movie.rentalCount}
-            onSeeMoreClick={() =>
-              console.log(`See more details for ${movie.title}`)
-            }
+            onSeeMoreClick={() => {
+              console.log(`See more details for ${movie.title}`);
+              handleSeeMoreClick(movie);
+            }}
           />
         ))}
       </div>
