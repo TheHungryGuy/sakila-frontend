72f27e42bdd90e0673a0eeaddfec7417811ffc49
Antonio Arce <antoarcejr@gmail.com>
2024-02-05 03:01:54 -0500

    basic layout setup for films

---
+++ src/components/feature_filmSearch/Datatable.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Button, Modal } from "flowbite-react";
+import { Label, TextInput } from "flowbite-react";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "age", headerName: "Age", type: "number", width: 90 },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
+const DataTable = () => {
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleCellClick = (params) => {
+    // You can customize this condition based on your needs
+    if (params.field === "fullName") {
+      console.log("Selected Row:", params.row);
+      setSelectedRow(params.row);
+      setOpenModal(true);
+    }
+  };
+
+  // const handleCloseModal = () => {
+  //   setOpenModal(false);
+  // };
+
+  return (
+    <div style={{ height: 600, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        onCellClick={handleCellClick}
+      />
+
+      <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+        <Modal.Header>
+          {selectedRow?.firstName} {selectedRow?.lastName}
+        </Modal.Header>
+
+        <Modal.Body>
+          <div className="space-y-6">
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              {/* Rental Count: {selectedMovie.rentalCount} */}
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Description: ipsum Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Omnis accusamus ad, qui ipsam nesciunt nam
+              minima modi voluptates debitis quam, iste fugit sed eligendi
+              laboriosam, voluptate perferendis accusantium quia soluta. Year
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Release: 20XX
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Rating: PG-13
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Special Features: Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ipsam quos voluptates quod quas dicta omnis
+              eligendi sit commodi non quisquam nisi, harum inventore cumque
+              ipsum architecto tempora nihil. Officia, explicabo.
+            </p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <div className="flex max-w-md flex-col gap-4">
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="username3"
+                  color="success"
+                  value="Customer Id"
+                />
+              </div>
+              <TextInput
+                id="username"
+                placeholder="e.g 0123456789"
+                required
+                color="success"
+                helperText={
+                  <>
+                    <span className="font-medium">Alright!</span> Movie
+                    Succesfully Rented!
+                  </>
+                }
+              />
+            </div>
+          </div>
+          <Button color="warning" onClick={() => setOpenModal(false)}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default DataTable;
--- src/pages/Films.jsx
+++ src/pages/Films.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import DataTable from "../components/feature_filmSearch/Datatable";
 
 const Films = () => {
   return (
     <div className="w-full pt-[4rem] py-[10rem] px-4 bg-white flex flex-col gap-24">
-      Films
+      <DataTable />
     </div>
   );
 };
