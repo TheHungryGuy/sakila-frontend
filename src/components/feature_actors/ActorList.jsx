9a3562e3889c5fd73add27ee55c0291c6defda85
Antonio Arce <antoarcejr@gmail.com>
2024-02-05 22:29:45 -0500

    Updated Actorlist modal

--- src/components/feature_actors/ActorList.jsx
+++ src/components/feature_actors/ActorList.jsx
@@ -1,42 +1,66 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Modal } from "flowbite-react";
-
 import ActorCard from "./ActorCard";
 
 const ActorList = () => {
-  const actors = [
-    { actorName: "Actor 1", movieCount: 20 },
-    { actorName: "Actor 2", movieCount: 15 },
-    { actorName: "Actor 3", movieCount: 10 },
-    { actorName: "Actor 4", movieCount: 8 },
-    { actorName: "Super Long Actor 5", movieCount: 5 },
-    // Test Actor List
-  ];
+  // const actors = [
+  //   { actorName: "Actor 1", movieCount: 20 },
+  //   { actorName: "Actor 2", movieCount: 15 },
+  //   { actorName: "Actor 3", movieCount: 10 },
+  //   { actorName: "Actor 4", movieCount: 8 },
+  //   { actorName: "Super Long Actor 5", movieCount: 5 },
+  //   // Test Actor List
+  // ];
 
+  const [actors, setActors] = useState([]);
+  const [topMovies, setTopMovies] = useState([]);
   const [openModal, setOpenModal] = useState(false);
   const [selectedActor, setSelectedActor] = useState({
-    actorName: "",
-    movieCount: 0,
+    full_name: "",
+    film_count: 0,
   });
+
+  useEffect(() => {
+    // Fetch actors data from Flask backend when the component mounts
+    fetch("http://127.0.0.1:5000/top_actors")
+      .then((response) => response.json())
+      .then((data) => setActors(data.top_actors))
+      .catch((error) => console.error("Error fetching actor data", error));
+  }, []);
 
   const handleSeeMoreClick = (actor) => {
     setSelectedActor(actor);
-    setOpenModal(true);
+
+    // Fetch top movies for the selected actor
+    fetch(`http://localhost:5000/top_movies_for_actor/${actor.actor_id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setTopMovies(data.top_movies);
+        setOpenModal(true);
+      })
+      .catch((error) => console.error("Error fetching top movies data", error));
   };
 
   return (
     <div className="w-full pt-[4rem] py-[10rem] px-4 bg-[#000300] flex flex-col gap-24">
       <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
-        <Modal.Header>{selectedActor.actorName}</Modal.Header>
+        <Modal.Header>{selectedActor.full_name}</Modal.Header>
         <Modal.Body>
           <div className="space-y-6">
             <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
               Top Films
             </p>
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              {selectedActor.movieCount}
-            </p>
+            {topMovies.map((movie, index) => (
+              <p
+                key={index}
+                className="text-base leading-relaxed text-gray-500 dark:text-gray-400"
+              >
+                {movie.title} - Rentals: {movie.rental_count}
+              </p>
+            ))}
+            {/* <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              {selectedActor.film_count}
+            </p> */}
           </div>
         </Modal.Body>
         <Modal.Footer>
@@ -54,10 +78,10 @@ const ActorList = () => {
         {actors.map((actor, index) => (
           <ActorCard
             key={index}
-            actorName={actor.actorName}
-            movieCount={actor.movieCount}
+            actorName={actor.full_name}
+            movieCount={actor.film_count}
             onSeeMoreClick={() => {
-              console.log(`See more details for ${actor.actorName}`);
+              console.log(`See more details for ${actor.full_name}`, actor);
               handleSeeMoreClick(actor);
             }}
           />
