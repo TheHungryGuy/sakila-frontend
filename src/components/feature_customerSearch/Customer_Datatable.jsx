35f134eeb940b6662799a0d9ea2a1187a87919c3
Antonio Arce <antoarcejr@gmail.com>
2024-02-07 01:13:04 -0500

    Added Customer Table

---
+++ src/components/feature_customerSearch/Customer_Datatable.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "customer_id", headerName: "Customer ID", width: 200 },
+  { field: "first_name", headerName: "First Name", width: 250 },
+  { field: "last_name", headerName: "Last Name", width: 300 },
+  { field: "email", headerName: "Email", width: 300 },
+];
+
+const Customer_Datatable = () => {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/customers")
+      .then((response) => response.json())
+      .then((data) => {
+        // Add a unique 'id' property to each row based on 'customer_id'
+        const customersWithId = data.map((customer) => ({
+          ...customer,
+          id: customer.customer_id,
+        }));
+        setCustomers(customersWithId);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div style={{ height: 800, width: "83%", margin: "auto" }}>
+      <DataGrid rows={customers} columns={columns} pageSize={10} />
+    </div>
+  );
+};
+
+export default Customer_Datatable;
--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Customer_Datatable from "../components/feature_customerSearch/Customer_Datatable";
 
 const Customer = () => {
   return (
     <div className="w-full pt-[4rem] py-[10rem] px-4 bg-white flex flex-col gap-24">
-      Customer
+      <Customer_Datatable />
     </div>
   );
 };
