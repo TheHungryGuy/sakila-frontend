0368aa623737aa3464055dacf3ef7007cc161405
Antonio Arce <antoarcejr@gmail.com>
2026-02-28 15:05:59 -0500

    Fixed cards for consistent alignments

--- src/components/feature_actors/ActorCard.jsx
+++ src/components/feature_actors/ActorCard.jsx
@@ -5,18 +5,21 @@ const ActorCard = ({ actorName, movieCount, onSeeMoreClick }) => {
   return (
     <div className="w-full shadow-xl shadow-amber-400 flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
       <MdPerson size={80} className="w-20 mx-auto mt-[-3rem] bg-yellow-400" />
-      <h2 className="text-2xl font-bold text-center py-6 text-white">
+      <h2 className="text-2xl font-bold text-center py-6 mt-auto text-white">
         {actorName}
       </h2>
-      <p className="text-center font-medium py-2 border-b mx-8 text-white">
-        Movie Count: {movieCount}
-      </p>
-      <button
-        onClick={onSeeMoreClick}
-        className="bg-yellow-400 lg:w-[150px] rounded-md font-medium my-6 mx-auto px-6 py-3"
-      >
-        See More
-      </button>
+
+      <div className="flex flex-col mt-auto">
+        <p className="text-center font-medium py-2 border-b mx-8 text-white">
+          Movie Count: {movieCount}
+        </p>
+        <button
+          onClick={onSeeMoreClick}
+          className="bg-yellow-400 lg:w-[150px] rounded-md font-medium my-6 mx-auto px-6 py-3"
+        >
+          See More
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/feature_movies/MovieCard.jsx
+++ src/components/feature_movies/MovieCard.jsx
@@ -8,16 +8,18 @@ const MovieCard = ({ title, rentalCount, onSeeMoreClick }) => {
         size={80}
         className="w-20 mx-auto mt-[-3rem] bg-yellow-400"
       />
-      <h2 className="text-2xl font-bold text-center py-6">{title}</h2>
-      <p className="text-center font-medium py-2 border-b mx-8">
-        Rental Count: {rentalCount}
-      </p>
-      <button
-        onClick={onSeeMoreClick}
-        className="bg-[#000300] text-yellow-400 lg:w-[150px] rounded-md font-medium my-6 mx-auto px-6 py-3"
-      >
-        See More
-      </button>
+      <h2 className="text-2xl font-bold text-center py-6 mt-auto">{title}</h2>
+      <div className="flex flex-col mt-auto">
+        <p className="text-center font-medium py-2 border-b mx-8">
+          Rental Count: {rentalCount}
+        </p>
+        <button
+          onClick={onSeeMoreClick}
+          className="bg-[#000300] text-yellow-400 lg:w-[150px] rounded-md font-medium my-6 mx-auto px-6 py-3"
+        >
+          See More
+        </button>
+      </div>
     </div>
   );
 };
